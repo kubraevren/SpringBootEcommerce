90dd90b2f94dc8bb507e52c31d9be3868d4ae9d4
Hatice Kübra Evren <haticeevren43@gmail.com>
2025-07-18 02:46:13 +0300

    ilk commit : Bismillah. Allahım nolur olsun amin. Spring projesi başlatıldı

---
+++ src/main/java/com/springprojem/demo/KubraevrenApplication.java
@@ -0,0 +1,13 @@
+package com.springprojem.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubraevrenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubraevrenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springprojem/demo/KubraevrenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springprojem.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubraevrenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
