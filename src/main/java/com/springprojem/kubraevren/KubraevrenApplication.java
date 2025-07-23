e60b61a08317c4f11487c19f976a67a9123f5b50
Hatice Kübra Evren <haticeevren43@gmail.com>
2025-07-24 01:43:29 +0300

    main sınıfı doğru pakete taşındı

--- src/main/java/com/springprojem/kubraevren/KubraevrenApplication.java
+++ src/main/java/com/springprojem/kubraevren/KubraevrenApplication.java
@@ -1,3 +1,4 @@
+package com.springprojem.kubraevren;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
